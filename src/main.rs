049d7a5a61d10718012752d8bfc7fc28ebb242b2
Tufusa <louptung@gmail.com>
2023-05-14 14:07:40 +0900

    feat: ウィンドウ

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use bevy::prelude::*;
+// use rand::prelude::*;
+
+const UNIT_WIDTH: u32 = 40;
+const UNIT_HEIGHT: u32 = 40;
+
+const X_LENGTH: u32 = 10;
+const Y_LENGTH: u32 = 18;
+
+const SCREEN_WIDTH: u32 = UNIT_WIDTH * X_LENGTH;
+const SCREEN_HEIGHT: u32 = UNIT_HEIGHT * Y_LENGTH;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Tetris".into(),
+                        resolution: (SCREEN_WIDTH as f32, SCREEN_HEIGHT as f32).into(),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+                .set(AssetPlugin {
+                    watch_for_changes: true,
+                    ..Default::default()
+                }),
+        )
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::WHITE,
+            ..Default::default()
+        },
+        transform: Transform {
+            scale: Vec3::new(20., 20., 0.),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
